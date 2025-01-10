f1eb190f4fec4773e3b2fc463121915111d2990b
Dani <daniel.taratiel@iesjulianmarias.es>
2025-01-10 11:43:52 +0100

    Suma de los 100 primeros números.

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Suma de los 100 primeros números");
+
+        int resultado = sumaPrimerosNumeros(100);
+
+        System.out.println("El resultado es: " + resultado);
+    }
+
+
+        public static int sumaPrimerosNumeros(int cantidad){
+            int suma = 0;
+            for (int i = 0; i < cantidad; i++) {
+                suma += i;
+            }
+            return suma;
+        }
+
+}
