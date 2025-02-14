7f04fff1ba98f38cccb1a93f10850a965b123daa
Dani <daniel.taratiel@iesjulianmarias.es>
2025-02-14 11:19:08 +0100

    Nuevas anotaciones y comparamos arrays.

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,9 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
 import java.io.*;
 
+// Nombre que aparece cuando se lanzan los test de esta clase.
+@DisplayName("Ejemplos de Test Unitarios en la Clase Main")
+// Sirve para poder ejecutar los test en un cierto orden.
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MainTest {
 
     //Variable para guardar la entrada de teclado.
@@ -37,20 +42,24 @@ class MainTest {
 
     @BeforeEach
     public void setUp() {
-        System.out.println("Me ejecuto antes de cada test. No soy Obligatorio");
+        // Imprimo en la consola original y si sale por pantalla pero el test no lo captura.
+        originalOut.println("Me ejecuto antes de cada test. No soy Obligatorio");
     }
 
     @AfterEach
     public void setDown() {
-        System.out.println("Me ejecuto después de cada test. No soy Obligatorio");
+        // Imprimo en la consola original y si sale por pantalla pero el test no lo captura.
+        originalOut.println("Me ejecuto después de cada test. No soy Obligatorio");
     }
 
 
     @Test
+    @DisplayName("Test del método main()")
+    // Este test se ejecuta el segundo.
+    @Order(2)
     void main() throws UnsupportedEncodingException {
         String inputString = "5\n";
         String outputString =
-                "Me ejecuto antes de cada test. No soy Obligatorio" + System.lineSeparator() +
                 "Introduce un número positivo" + System.lineSeparator() +
                 "El resultado es: 15" + System.lineSeparator();
 
@@ -63,6 +72,11 @@ class MainTest {
     }
 
     @Test
+    @DisplayName("Test del método sumaPrimerosNumeros()")
+    // Este test se ejecuta primero.
+    @Order(1)
+    @EnabledOnOs(value = {OS.LINUX, OS.WINDOWS})
+    @EnabledOnJre(value = {JRE.JAVA_18})
     void sumaPrimerosNumeros() {
         assertAll(
                 () -> assertEquals(1, Main.sumaPrimerosNumeros(1), "Suma 1 número"),
@@ -70,6 +84,24 @@ class MainTest {
                 () -> assertEquals(5050, Main.sumaPrimerosNumeros(100), "Suma 10 números"),
                 () -> assertThrows(IllegalArgumentException.class, () -> Main.sumaPrimerosNumeros(-1), "Cantidad Negativa")
         );
+    }
+
+    @Test
+    @DisplayName("Test para comparar arrays")
+    @EnabledForJreRange(min = JRE.JAVA_8, max = JRE.JAVA_11) // Podemos activar o desactivar el test para un rango de JRE
+    public void arrayEquals() {
+        // Arrange, preparamos el test
+        int[] valor = {1,2,3,4,5};
+        int[] esperado = {1,2,3,4,5};
+        // Actuamos, en este caso no hacemos nada
+        System.out.println("array == array");
+
+        // Comprobamos resulatdos de dos maneras.
+        assertAll(
+                () -> assertEquals(esperado, valor, "Las variables no son iguales"),
+                () -> assertArrayEquals(esperado, valor, "Los array no son iguales")
+        );
+
     }
 
 
